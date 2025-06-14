efddb6d1379203d3ee06858d3f5f6cf826ba534f
JasonCrk <emerzonkolki@gmail.com>
2025-06-14 13:35:22 -0500

    feat: landing page complete

---
+++ src/app/shared/pages/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { LandingHeroComponent } from './ui/landing-hero/landing-hero.component';
+
+@Component({
+  selector: 'gow-landing-page',
+  imports: [LandingHeroComponent],
+  templateUrl: './landing.component.html',
+})
+export class LandingPageComponent {}
---
+++ src/app/shared/pages/landing/ui/landing-hero/landing-hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'gow-landing-hero',
+  imports: [NgOptimizedImage, RouterLink],
+  templateUrl: './landing-hero.component.html',
+})
+export class LandingHeroComponent {}
