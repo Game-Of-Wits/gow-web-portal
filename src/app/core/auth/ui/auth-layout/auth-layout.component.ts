04354c98ea08c953fe1ba93e6cea7c31d54dd572
JasonCrk <emerzonkolki@gmail.com>
2025-06-16 13:39:23 -0500

    feat(auth-layout): Logo link

--- src/app/core/auth/ui/auth-layout/auth-layout.component.ts
+++ src/app/core/auth/ui/auth-layout/auth-layout.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core'
 
 import { NgOptimizedImage } from '@angular/common'
 
-import { RouterOutlet } from '@angular/router'
+import { RouterLink, RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'gow-auth-layout',
-  imports: [RouterOutlet, NgOptimizedImage],
+  imports: [RouterOutlet, RouterLink, NgOptimizedImage],
   templateUrl: './auth-layout.component.html'
 })
 export class AuthLayoutComponent {}
