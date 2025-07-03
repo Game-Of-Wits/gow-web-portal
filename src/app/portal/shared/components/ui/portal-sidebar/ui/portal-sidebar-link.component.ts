0e2a4dce38008b1131d67eaacda9e0c3eef3d69d
JasonCrk <73272783+JasonCrk@users.noreply.github.com>
2025-07-03 18:59:58 -0500

    fix(portal-sidebar-link.component.ts): active link

--- src/app/portal/shared/components/ui/portal-sidebar/ui/portal-sidebar-link.component.ts
+++ src/app/portal/shared/components/ui/portal-sidebar/ui/portal-sidebar-link.component.ts
@@ -1,32 +1,38 @@
-import { Component, OnInit, inject, input, signal } from '@angular/core'
-import { Router, RouterLink } from '@angular/router'
+import { Component, inject, input } from '@angular/core'
+import { Router, RouterLink, RouterLinkActive } from '@angular/router'
 
 @Component({
   selector: 'gow-portal-sidebar-link',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
+  styles: `
+    .sidebar-link-active {
+      color: white;
+      background-color: var(--p-primary-500);
+
+      &:hover {
+        background-color: var(--p-primary-600);
+      }
+
+      & > span {
+        font-weight: bold;
+      }
+    }
+  `,
   template: `
     <a
       [routerLink]="linkHref()"
-      class="px-[16px] py-[10px] rounded-xl text-base leading-none gap-2.5 flex items-center transition-colors {{
-          isActive()
-            ? 'text-white bg-primary-500 hover:bg-primary-600'
-            : 'text-dark hover:bg-surface-100'
-        }}"
+      routerLinkActive="sidebar-link-active"
+      class="px-[16px] py-[10px] rounded-xl text-base leading-none gap-2.5 flex items-center transition-colors text-dark hover:bg-surface-100"
     >
       <ng-content select="[icon]"></ng-content>
-      <span class="leading-none text-base {{ isActive() && 'font-bold' }}">
+      <span class="leading-none text-base">
         <ng-content></ng-content>
       </span>
     </a>
   `
 })
-export class PortalSidebarLinkComponent implements OnInit {
+export class PortalSidebarLinkComponent {
   public router = inject(Router)
 
   public linkHref = input.required<string>({ alias: 'href' })
-  public isActive = signal<boolean>(false)
-
-  ngOnInit(): void {
-    this.isActive.set(this.router.url.startsWith(this.linkHref()))
-  }
 }
