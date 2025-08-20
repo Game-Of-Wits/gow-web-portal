e196a734a90f1805aa793b6e2c68e35884b4e96d
JasonCrk <73272783+JasonCrk@users.noreply.github.com>
2025-08-19 20:12:19 -0500

    feat(environment.ts): Deploy config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
+    appId: process.env['NG_APP_FIREBASE_APP_ID'],
+    databaseURL: process.env['NG_APP_FIREBASE_DATABASE_URL'],
+    storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET_URL'],
+    apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
+    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
+    messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
+    measurementId: process.env['NG_APP_FIREBASE_MEASUREMENT_ID']
+  }
 }
